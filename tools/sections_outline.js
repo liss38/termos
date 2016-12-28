df77124078bdee8d7171564a1c84436997f5e17c
Max Salnikov <liss38@yandex.ru>
2016-12-28 17:40:03 +0300

    js-tools :: sections-outline

---
+++ tools/sections_outline.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.section').forEach(function (item, index, array) {
+	item.style.outline = '2px solid yellow';
+});
+
+
+document.querySelectorAll('.section__inner').forEach(function (item, index, array) {
+	item.style.outline = '2px solid yellow';
+});
